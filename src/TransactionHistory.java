fcf000fd8e16c9837fb53f8f89f2222b5ab6d354
BT_1N3_30 <sample@bths.edu>
2024-01-04 09:28:38 -0500

    transaction history

--- src/TransactionHistory.java
+++ src/TransactionHistory.java
@@ -15,6 +15,31 @@ public class TransactionHistory {
     }
 
 
-    public void
+    public String newAccTransID() {
+        accTransactionNum++;
+        String printedNum = "" + accTransactionNum;
+        if (accTransactionNum < 10) {
+            printedNum = "000" + accTransactionNum;
+        } if (accTransactionNum < 100) {
+            printedNum = "00" + accTransactionNum;
+        } if (accTransactionNum < 1000) {
+            printedNum = "0" + accTransactionNum;
+        }
+
+        return "A" + printedNum;
+    }
+    public String newSecTransID() {
+        secTransactionNum++;
+        String printedNum = "" + secTransactionNum;
+        if (secTransactionNum < 10) {
+            printedNum = "000" + secTransactionNum;
+        } if (secTransactionNum < 100) {
+            printedNum = "00" + secTransactionNum;
+        } if (secTransactionNum < 1000) {
+            printedNum = "0" + secTransactionNum;
+        }
+
+        return "S" + printedNum;
+    }
 
 }
